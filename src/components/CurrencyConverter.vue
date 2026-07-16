5e6f0a5876b94c44a56ad54492020a6440147703
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-07-16 23:24:24 +0200

    feat: add CurrencyConverter base template structure

---
+++ src/components/CurrencyConverter.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <section class="currency-converter" aria-labelledby="currency-converter-heading">
+        <h2 id="currency-converter-heading">Conversor de divisas</h2>
+    </section>
+</template>
+
+<style lang="scss" scoped></style>
