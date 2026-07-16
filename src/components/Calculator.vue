4c829205bc549c59bbb4706f7c0392410a32f27d
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-07-16 22:53:39 +0200

    feat: add calculator base template structure

---
+++ src/components/Calculator.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <section class="calculator" aria-labelledby="calculator-heading">
+        <h2 id="calculator-heading">Calculadora</h2>
+    </section>
+</template>
+
+<style lang="scss" scoped></style>
