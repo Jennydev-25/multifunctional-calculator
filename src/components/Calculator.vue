31e806167a02e6b4e957539f4b55c4e94681ea69
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-07-16 22:57:41 +0200

    feat: add calculator display output

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,6 +4,8 @@
 <template>
     <section class="calculator" aria-labelledby="calculator-heading">
         <h2 id="calculator-heading">Calculadora</h2>
+
+        <output class="calculator__display" aria-live="polite">0</output>
     </section>
 </template>
 
