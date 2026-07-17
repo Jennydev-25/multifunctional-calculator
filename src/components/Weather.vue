752548d616b2d3354591d9ead79a00e841558cc9
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-07-17 17:18:55 +0200

    feat: add Weather base template structure

---
+++ src/components/Weather.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <section class="weather" aria-labelledby="weather-heading">
+        <h2 id="weather-heading">El tiempo</h2>
+    </section>
+</template>
+
+<style lang="scss" scoped></style>
