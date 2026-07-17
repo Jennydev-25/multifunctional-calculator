2083a8b382c6ec4b666e42e30e3d9083a3aab5cd
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-07-17 17:07:00 +0200

    feat: add amount and origin currency fields to currency converter

--- src/components/CurrencyConverter.vue
+++ src/components/CurrencyConverter.vue
@@ -4,6 +4,20 @@
 <template>
     <section class="currency-converter" aria-labelledby="currency-converter-heading">
         <h2 id="currency-converter-heading">Conversor de divisas</h2>
+
+        <form>
+            <div class="currency-converter__field">
+                <label for="amount-from">Cantidad</label>
+                <input type="number" id="amount-from" name="amount-from" min="0" />
+
+                <label for="currency-from">Divisa de origen</label>
+                <select id="currency-from" name="currency-from">
+                    <option value="EUR">Euro (€)</option>
+                    <option value="USD">Dólar ($)</option>
+                    <option value="JPY">Yen (¥)</option>
+                </select>
+            </div>
+        </form>
     </section>
 </template>
 
