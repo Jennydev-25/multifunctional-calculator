cf70f88ccdfba82cf001f65240ae9f2c7dc230b8
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-07-19 17:03:41 +0200

    feat: implement inputDigit for empty display

--- src/composables/useCalculator.js
+++ src/composables/useCalculator.js
@@ -3,5 +3,13 @@ import { ref } from 'vue';
 export function useCalculator() {
     const display = ref('0');
 
-    return { display };
+    function inputDigit(digit) {
+        if (display.value === '0') {
+            display.value = digit;
+        } else {
+            display.value = display.value + digit;
+        }
+    }
+
+    return { display, inputDigit };
 }
